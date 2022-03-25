b2c64a291870a65ddf1b87d33e7681edaf488a4b
David Marcelo <dmarcelo1226@gmail.com>
2022-03-25 16:01:46 -0700

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,23 +1,23 @@
 << << << < HEAD
 
 
-def hello_world(state):
-    print(f'Hello World {state}!')
-
-
-def main():
-    hello_world('New York')
-
-
-== == == =
-
-
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
+
+
+== == == =
+
+
+def hello_world(city, state):
+    print(f'Hello World {city}!')
+
+
+def main():
+    hello_world('NYC')
 
 
 >>>>>> > d0d68045edd2a0775f3971fcce26500dd869d1be
